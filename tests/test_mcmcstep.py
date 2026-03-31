5a2b1a94a704c7c07c81ca7732e12fe555db8a19
Gattocrucco <info@giacomopetrillo.com>
2026-03-31 10:13:45 +0200

    fix: sharding comparison

--- tests/test_mcmcstep.py
+++ tests/test_mcmcstep.py
@@ -882,7 +882,7 @@ def check_same_structure(x: PyTree, y: PyTree) -> None:
     def check(_path: KeyPath, x: Array, y: Array) -> None:
         assert x.shape == y.shape
         assert x.dtype == y.dtype
-        assert x.sharding == y.sharding
+        assert x.sharding.is_equivalent_to(y.sharding, x.ndim)
 
     tree.map_with_path(check, x, y)
 
